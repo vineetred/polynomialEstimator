e97a016d5f3bb784dc72bed39f24c7e914d9a806
Vineet Reddy <hotshot2797@gmail.com>
2019-02-02 13:01:44 +0530

    Plots properly now

--- assign.py
+++ assign.py
@@ -7,35 +7,35 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
 
-#DATA
+#DATA UNPACKING
 fileopen = open('hw1data.txt') 
 x,y = np.loadtxt(fileopen,usecols=(0,1), unpack=True)
+#DIVIDING THE DATA INTO TRAINING AND TESTING
 X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.25)
 
+#THIS FIXES A BUG THE MESSES UP THE ORDER OF POINTS AFTER SPLIT - TRAINING
 zipXTrain = zip(X_train,y_train)
 x_list = list(zipXTrain)
 x_list.sort()
 X_train, y_train = zip(*x_list)
-
+#RESHAPING AFTER FIX - TRAINING
 X_train = np.asarray(X_train)
 X_train = np.reshape(X_train,(-1,1))
 y_train = np.asarray(y_train)
 y_train = np.reshape(y_train,(-1,1))
 
+#THIS FIXES A BUG THE MESSES UP THE ORDER OF POINTS AFTER SPLIT - TESTING
+zipXTest = zip(X_test,y_test)
+y_list = list(zipXTest)
+y_list.sort()
+X_test,y_test = zip(*y_list)
+#RESHAPING AFTER FIX - TESTING
+X_test = np.asarray(X_test)
+X_test = np.reshape(X_test,(-1,1))
+y_test = np.asarray(y_test)
+y_test = np.reshape(y_test,(-1,1))
 
-# X_train = x
-# y_train = y
-# X_test = x
-# y_test = y
-# transforming the data to include another axis
-# X_test,X_train = X_test,X_train[:, np.newaxis]
-# X_train = X_train[:, np.newaxis]
-X_test = X_test[:, np.newaxis]
-# x.reshape(,51)
-# x = x.reshape(1,-1)
-# y_train = y_train[:, np.newaxis]
-y_test = y_test[:, np.newaxis]
-# y = y.reshape(1,-1)
+
 rmse_arr = []
 model = LinearRegression()
 def poly_train(deg):
@@ -53,6 +53,8 @@ def poly_train(deg):
     print(rmse)
     plt.scatter(X_train, y_train, s=10)
     plt.plot(X_train, y_pred, color='r')
+    plt.title("Training - The degree is "+str(deg))
+    plt.xlabel("RMSE = "+str(rmse))
     plt.show()
 
 def poly_test(deg):
@@ -61,12 +63,13 @@ def poly_test(deg):
     y_testpred = model.predict(x_test)
     rmse_test = np.sqrt(mean_squared_error(y_test,y_testpred))
     print(rmse_test)
-    plt.xlabel('TESTING')
+    plt.title("Testing - The degree is "+str(deg))
     plt.scatter(X_test, y_test, s=10)
     plt.plot(X_test, y_testpred, color='r')
+    plt.xlabel("RMSE = " + str(rmse_test))
     plt.show()
 
-many = input("Enter the polynomial degrees").split(" ")
+many = input("Enter the polynomial degrees - ").split(" ")
 newrmse = []
 for i in many:
     newrmse.append(int(i))
