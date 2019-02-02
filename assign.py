7c98ca5ce500e1112fec85a6c5993e5145ae210a
Vineet Reddy <hotshot2797@gmail.com>
2019-02-02 13:41:24 +0530

    Modularity

--- assign.py
+++ assign.py
@@ -13,28 +13,22 @@ x,y = np.loadtxt(fileopen,usecols=(0,1), unpack=True)
 #DIVIDING THE DATA INTO TRAINING AND TESTING
 X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.25)
 
+def bugFix(X1,Y1):
+    zipXTrain = zip(X1,Y1)
+    x_list = list(zipXTrain)
+    x_list.sort()
+    X1, Y1 = zip(*x_list)
+    #RESHAPING AFTER FIX - TRAINING
+    X1 = np.asarray(X1)
+    X1 = np.reshape(X1,(-1,1))
+    Y1 = np.asarray(Y1)
+    Y1 = np.reshape(Y1,(-1,1))
+    return X1, Y1
+
 #THIS FIXES A BUG THE MESSES UP THE ORDER OF POINTS AFTER SPLIT - TRAINING
-zipXTrain = zip(X_train,y_train)
-x_list = list(zipXTrain)
-x_list.sort()
-X_train, y_train = zip(*x_list)
-#RESHAPING AFTER FIX - TRAINING
-X_train = np.asarray(X_train)
-X_train = np.reshape(X_train,(-1,1))
-y_train = np.asarray(y_train)
-y_train = np.reshape(y_train,(-1,1))
-
+X_train, y_train = bugFix(X_train,y_train)
 #THIS FIXES A BUG THE MESSES UP THE ORDER OF POINTS AFTER SPLIT - TESTING
-zipXTest = zip(X_test,y_test)
-y_list = list(zipXTest)
-y_list.sort()
-X_test,y_test = zip(*y_list)
-#RESHAPING AFTER FIX - TESTING
-X_test = np.asarray(X_test)
-X_test = np.reshape(X_test,(-1,1))
-y_test = np.asarray(y_test)
-y_test = np.reshape(y_test,(-1,1))
-
+X_test, y_test = bugFix(X_test,y_test)
 
 rmse_arr = []
 rmse_test = []
--- testing.py
+++ testing.py
@@ -26,3 +26,35 @@ XX_Train = np.reshape(XX_Train,(-1,1))
 print(XX_Train)
 # print(type(X_test))
 # print(XX_Train.shape)
+
+def bugFix(X1,Y1):
+    zipXTrain = zip(X1,Y1)
+    x_list = list(zipXTrain)
+    x_list.sort()
+    X1, Y1 = zip(*x_list)
+    #RESHAPING AFTER FIX - TRAINING
+    X1 = np.asarray(X1)
+    X1 = np.reshape(X1,(-1,1))
+    Y1 = np.asarray(Y1)
+    Y1 = np.reshape(Y1,(-1,1))
+    return X1, Y1
+
+# zipXTrain = zip(X_train,y_train)
+# x_list = list(zipXTrain)
+# x_list.sort()
+# X_train, y_train = zip(*x_list)
+# #RESHAPING AFTER FIX - TRAINING
+# X_train = np.asarray(X_train)
+# X_train = np.reshape(X_train,(-1,1))
+# y_train = np.asarray(y_train)
+# y_train = np.reshape(y_train,(-1,1))
+
+# zipXTest = zip(X_test,y_test)
+# y_list = list(zipXTest)
+# y_list.sort()
+# X_test,y_test = zip(*y_list)
+# #RESHAPING AFTER FIX - TESTING
+# X_test = np.asarray(X_test)
+# X_test = np.reshape(X_test,(-1,1))
+# y_test = np.asarray(y_test)
+# y_test = np.reshape(y_test,(-1,1))
